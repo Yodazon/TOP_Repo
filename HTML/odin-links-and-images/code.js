07fea6f9b1261811e6dce2c31d4a28c2d1b285b6
Yodazon <michael.karas96@gmail.com>
2023-07-28 09:17:58 -0400

    new project
    working on project for odin project

--- HTML/odin-links-and-images/code.js
+++ HTML/odin-links-and-images/code.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    var buttonsWanted = 10;
-    var doc = document;
-    var docFrag = document.createDocumentFragment();
-  
-    for(var x = 0; x < buttonsWanted; x++){
-      var button = doc.createElement('button');
-      button.setAttribute('text', 'yourtext');
-      docFrag.appendChild(button);
+function about_link(){
+    const linkElement = document.querySelector('.js-about-tag')
+
+
+    if(linkElement.classList = 'about-Unclicked'){
+        linkElement.classList.add('about-clicked')
+        linkElement.classList.remove('about-Unclicked')
+    }else{
+        linkElement.classList.remove('about-clicked')
+        linkElement.classList.add('about-Unclicked')
     }
-  
-    doc.getElementById('container').appendChild(docFrag);
-  });
+
+
+
+
+}
